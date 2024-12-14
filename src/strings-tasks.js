ea23dbde3358fe4c584814555c68b704670540a7
Almaz0n <1iv2an3ov4@gmail.com>
2024-12-14 21:40:48 +0500

    feat: decided 13 tasks two time

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -381,7 +381,7 @@ function getStringFromTemplate(/* firstName, lastName */) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/*value*/) {
+function extractNameFromTemplate(/* value */) {
   throw new Error('Not implemented');
 }
 
